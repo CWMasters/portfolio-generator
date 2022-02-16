296eb8ade00e9864b086f01a1964696b8415b610
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-02-16 05:01:39 -0700

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    // this.....
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+console.log(profileDataArgs);
 
-console.log('================');
+const [name, github] = profileDataArgs;
 
-// is this the same as this...
-profileDataArr.forEach(profileItem => console.log(profileItem)); 
-}; 
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+// const printProfileData = profileDataArr => {
+//     // this.....
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+
+// console.log('================');
+
+// // is this the same as this...
+// profileDataArr.forEach(profileItem => console.log(profileItem)); 
+// }; 
+
+// printProfileData(profileDataArgs);
