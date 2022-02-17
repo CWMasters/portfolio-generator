870d5d741e3c92be9f5b79028939b75e121ae8cb
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-02-17 04:37:20 -0700

    inquirer added

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(names, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-console.log(name, github);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+// const profileDataArgs = process.argv.slice(2);
+
+// console.log(profileDataArgs);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
 
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-module.exports = (name, github) => {
+module.exports = (names, github) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -11,7 +11,7 @@ module.exports = (name, github) => {
     </head>
     
     <body>
-      <h1>${name}</h1>
+      <h1>${names}</h1>
       <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
